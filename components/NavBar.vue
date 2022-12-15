cec7306ba3a0ea57ca705a6b235f5f5b417a19a9
Fatih Yurek <fatihenaz@gmail.com>
2022-12-15 14:06:08 +0300

    config: fontawesome added

---
+++ components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-app-bar app>
+      <v-app-bar-nav-icon class="d-flex d-sm-none" @click="drawer = true">
+      </v-app-bar-nav-icon>
+      <v-toolbar-title>My Blog</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text icon
+        ><v-icon class="mr-1">fa-magnifying-glass</v-icon>
+      </v-btn>
+      <v-icon>fa-edit</v-icon>
+      <v-btn text icon>
+        <v-icon>fa elipsis-vertical</v-icon>
+      </v-btn>
+      <v-btn>Search</v-btn>
+    </v-app-bar>
+    <!-- Add a navigation bar -->
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group>
+          <v-list-item v-for="(item, index) in items" :key="index">
+            <v-list-item-title @click="tab = index">{{
+              item
+            }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <!-- Navigation bar ends -->
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    tab: null,
+    items: ['Home', 'About', 'Examples', 'Contact'],
+  }),
+}
+</script>
+
+<style></style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-import colors from 'vuetify/es5/util/colors'
-
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -18,7 +16,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['@fortawesome/fontawesome-free/css/all.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -49,19 +47,13 @@ export default {
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
+    defaultAssets: { icons: 'fa' },
+    icons: {
+      iconfont: 'fa',
+    },
+    treeShake: true,
     theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
-        },
-      },
+      dark: false,
     },
   },
 

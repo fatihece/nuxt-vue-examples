4a69de0208ad597dd70dadb4ff70b8fa38beb82e
Fatih Yurek <fatihenaz@gmail.com>
2022-12-17 21:49:08 +0300

    feat: google map added to contact section

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,8 +1,16 @@
 <template>
-  <v-container>
+  <v-container class="mx-auto">
     <v-row>
       <v-col>
-        <h2>This is Contact page</h2>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6023.145783935384!2d29.081432575957262!3d40.99083241721006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cac7ce887d5fa3%3A0xa17fddd1c5b43ca4!2sYeni%20Sahra%2C%2034746%20Ata%C5%9Fehir%2F%C4%B0stanbul!5e0!3m2!1sen!2str!4v1671302641186!5m2!1sen!2str"
+          width="100%"
+          height="500"
+          style="border: 0"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
       </v-col>
     </v-row>
   </v-container>

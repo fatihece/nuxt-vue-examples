f66a02ff2a32cd6b939979a00d81d31117081c25
Fatih Yurek <fatihenaz@gmail.com>
2022-12-25 16:17:53 +0300

    feat: middleware added

---
+++ middleware/redirect.js
@@ -0,0 +1,4 @@
+export default function ({ store, redirect }) {
+  // return redirect(`${process.env.NUXT_APP_BASE_URL}/payment`)
+  return redirect('https://nuxtjs.org/')
+}
---
+++ pages/middleware/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>Payment page</div>
+</template>
+<script>
+export default {
+  middleware: 'redirect',
+}
+</script>

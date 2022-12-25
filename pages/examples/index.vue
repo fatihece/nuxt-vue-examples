b12543621a52f83327980173ba79170b26f6a39b
Fatih Yurek <fatihenaz@gmail.com>
2022-12-25 15:31:08 +0300

    feat: expire date formula added

--- pages/examples/index.vue
+++ pages/examples/index.vue
@@ -1,15 +1,53 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col>
-        <h2>This is Examples page</h2>
-      </v-col>
-    </v-row>
+  <v-container fluid pa-0 ma-0>
+    <v-row
+      ><v-col class="pa-0">
+        <section class="orange-banner">
+          <v-container fluid pa-0 ma-0>
+            <v-row justify="center">
+              <v-col md="8" class="">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi,
+                  voluptatum.
+                </p>
+                <p class="font-italic" style="font-size: 24px">
+                  Expires {{ expireDate }}
+                </p>
+              </v-col>
+            </v-row>
+          </v-container>
+        </section>
+      </v-col></v-row
+    >
   </v-container>
 </template>
 
 <script>
-export default {}
+export default {
+  data: () => ({}),
+  computed: {
+    expireDate() {
+      const dt = new Date()
+      dt.setDate(dt.getDate() + 2)
+      return (
+        Number(dt.getMonth()) + 1 + '/' + dt.getDate() + '/' + dt.getFullYear()
+      )
+    },
+  },
+}
 </script>
 
-<style></style>
+<style scoped>
+.orange-banner {
+  background-color: #fea054;
+  text-align: center;
+  font-family: 'Playfair Display', serif;
+  padding: 12px 40px;
+}
+.orange-banner p {
+  font-size: 32px;
+  color: #000;
+  margin-bottom: 0;
+  font-weight: 600;
+}
+</style>
